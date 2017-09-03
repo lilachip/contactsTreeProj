e120ff7ae2ba473eb9d404bb024b2cde52ad3484
lilach <lilach.karavani@gmail.com>
2017-09-03 20:19:36 +0300

    small changes

--- components/contactsTree.component.js
+++ components/contactsTree.component.js
@@ -1,15 +1,17 @@
 
 
 var contactsTreeController = function(contactsTreeService) {
+    //keeping the scope obj localy
 	var self = this;
 
     self.isActive = [];
+    //gets the data from the service - at the moment staticly...
     self.contactsList = contactsTreeService.data;
-   
+    //toggle items header
     self.toggleGroup = function(contactId) {
         self.isActive[contactId] = !self.isActive[contactId];
     }  
-
+   //shows elements according to group's choice
     self.isGroupCanShow = function(group) {
     	return group.contacts && self.isActive[group.id];
     }  
